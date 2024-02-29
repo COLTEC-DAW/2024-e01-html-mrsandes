d79b66367412d860787e7017ef8558b4190da50e
João Eduardo Montandon <edu.montandon@gmail.com>
2024-02-29 01:14:57 -0300

    Configured cypress autograding workflow

--- .github/actions/html-elements/cypress.config.js
+++ .github/actions/html-elements/cypress.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    e2e: {
-      baseUrl: "http://localhost:8080",
-      specPattern: "cypress/integration/**/*.test.js",
-      video: true,
-      screenshotsFolder: "cypress/screenshots",
-      videosFolder: "cypress/videos",
-      viewportWidth: 1280,
-      viewportHeight: 720,
-      supportFile: false,
-      reporter: 'junit',
-    }
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    specPattern: `${process.env.CYPRESS_PATH}/cypress/integration/**/*.test.js`,
+    video: true,
+    screenshotsFolder: `${process.env.CYPRESS_PATH}/cypress/screenshots`,
+    videosFolder: `${process.env.CYPRESS_PATH}/cypress/videos`,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    supportFile: false,
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: `${process.env.CYPRESS_PATH}/cypress/reports/report.xml`
+    },
   }
+}
